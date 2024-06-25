d78bfe9a5ca0986a4ac6b035406e45cfade7fdde
Christopher <voschristopher53@gmail.com>
2024-06-25 11:59:35 +0200

    added selectors and model content

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
   
     // Hide the modal initially
     modal.style.display = 'none';
-  
+
     cardForm.addEventListener('submit', function (e) {
       e.preventDefault();
   
       // 🚨 Get input values
       const studentNameInput = document.getElementById('studentName');
-      const personalMessageInput = ;
-      const courseNameInput = ; 
+      const personalMessageInput = document.getElementById('personalMessage');
+      const courseNameInput = document.getElementById('courseName'); 
   
       const studentName = studentNameInput.value;
       const personalMessage = personalMessageInput.value;
@@ -25,8 +25,15 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   
       // 🚨 Generate certificate content dynamically
-      certificateContent. = `
+      certificateContent.innerHTML = `
+      <h2>Certificate of Achievement</h2>
+      <p>this is to certify that</p>
       <h3>${studentName}</h3>
+      <p>has almost completed</p>
+      <h3>${courseName}</h3>
+      <p>with legendary perseverance and world-class bad-assery, for never giving up 🏆</p>
+      <img src="./logo.png" alt="giphy">
+      <p>${personalMessage}</p>
     `;
     
       //  Display the modal
@@ -39,8 +46,8 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.('', function () {
-      
+    closeModal.addEventListener('click', function () {
+      modal.style.display = 'none';
     });
   });
   
